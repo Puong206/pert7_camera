bfb749778a2e90df3015abc2a7740fac5c7737e6
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-17 10:17:18 +0700

    feat: menambahkan daftar kamera pada kelas CameraBloc

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -1,7 +1,10 @@
+import 'package:camera/camera.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pert7_camera/bloc/camera_event.dart';
 import 'package:pert7_camera/bloc/camera_state.dart';
 
 class CameraBloc extends Bloc<CameraEvent, CameraState> {
+  late List<CameraDescription> _cameras;
+
   
 }
