d324f5aa25a25f290fc11e3979742555df868d79
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-17 10:16:46 +0700

    feat: menambahkan kelas CameraBloc untuk pengelolaan status kamera

---
+++ lib/bloc/camera_bloc.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pert7_camera/bloc/camera_event.dart';
+import 'package:pert7_camera/bloc/camera_state.dart';
+
+class CameraBloc extends Bloc<CameraEvent, CameraState> {
+  
+}
