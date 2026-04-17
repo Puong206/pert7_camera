40e3fc48ed71028957769f7b19308fe4d0d64ad6
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-17 10:05:07 +0700

    feat: menambahkan kelas CameraReady untuk pengelolaan status kamera

--- lib/bloc/camera_state.dart
+++ lib/bloc/camera_state.dart
@@ -1,4 +1,32 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+
 sealed class CameraState {}
 
 final class CameraInitial extends CameraState {}
 
+// CameraReady adalah state yang menunjukkan bahwa kamera sudah siap digunakan.
+final class CameraReady extends CameraState {
+  // controller: Mengontrol kamera, seperti memulai preview atau mengambil gambar.
+  final CameraController controller;
+  // selectedIndex: Menunjukkan kamera yang sedang aktif (misalnya, kamera depan atau belakang).
+  final int selectedIndex;
+  // flashMode: Menunjukkan mode flash saat ini (misalnya, auto, on, off).
+  final FlashMode flashMode;
+  // imageFile: Menyimpan file gambar yang baru saja diambil, jika ada.
+  final File? imageFile;
+  // snackbarMessage: Pesan yang akan ditampilkan di snackbar, jika ada.
+  final String? snackbarMessage;
+
+  CameraReady({
+    // Semua parameter ini diperlukan untuk memastikan bahwa state ini 
+    // memiliki semua informasi yang dibutuhkan untuk menampilkan UI kamera dengan benar.
+    required this.controller,
+    required this.selectedIndex,
+    required this.flashMode,
+    this.imageFile,
+    this.snackbarMessage,
+  });
+
+}
